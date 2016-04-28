dda0163495cd99722dfe1c478c18ef98a8ed496c
Ian Kennedy <exposure@gmail.com>
2016-04-28 12:39:57 -0400

    Namespaces Sass color variables

---
+++ gulp.js
@@ -0,0 +1,5 @@
+var styleguide = require('devbridge-styleguide');
+
+gulp.task('start-styleguide', function () {
+  styleguide.startServer();
+});
---
+++ min/gulp-min.js
@@ -0,0 +1,1 @@
+var styleguide=require("devbridge-styleguide");gulp.task("start-styleguide",function(){styleguide.startServer()});
