bcab6cb3e0f43c5b06b234dbc9cefe10605a3e39
Ian Kennedy <exposure@gmail.com>
2016-04-28 17:17:49 -0400

    Removed scripts and comments from header and footer

--- wp-content/themes/sorbaatlanta/footer.php
+++ wp-content/themes/sorbaatlanta/footer.php
@@ -26,38 +26,6 @@
     </ul>
   </div><!-- end footer links -->
 </footer>
-  <script>
-    jQuery(window).on("load resize", function (e) {
-
-      var more = document.getElementById("js-centered-more");
-      var windowWidth = jQuery(window).width();
-      var moreLeftSideToPageLeftSide = jQuery(more).offset().left;
-      var moreLeftSideToPageRightSide = windowWidth - moreLeftSideToPageLeftSide;
-
-      if (moreLeftSideToPageRightSide < 330) {
-        jQuery("#js-centered-more .submenu .submenu").removeClass("fly-out-right");
-        jQuery("#js-centered-more .submenu .submenu").addClass("fly-out-left");
-      }
-
-      if (moreLeftSideToPageRightSide > 330) {
-        jQuery("#js-centered-more .submenu .submenu").removeClass("fly-out-left");
-        jQuery("#js-centered-more .submenu .submenu").addClass("fly-out-right");
-      }
-
-      var menuToggle = jQuery("#js-centered-navigation-mobile-menu").unbind();
-      jQuery("#js-centered-navigation-menu").removeClass("show");
-
-      menuToggle.on("click", function (e) {
-        e.preventDefault();
-        jQuery("#js-centered-navigation-menu").slideToggle(function () {
-          if (jQuery("#js-centered-navigation-menu").is(":hidden")) {
-            jQuery("#js-centered-navigation-menu").removeAttr("style");
-          }
-        });
-      });
-    });
-  </script>
-
 <?php wp_footer(); ?>
 </body>
 </html>
--- wp-content/themes/sorbaatlanta/header.php
+++ wp-content/themes/sorbaatlanta/header.php
@@ -25,25 +25,6 @@
       
           wp_nav_menu($menu_args);
         ?>
-<!--
-        <ul id="js-centered-navigation-menu" class="centered-navigation-menu show">
-        <li class="nav-link"><a href="javascript:void(0)">ABOUT</a></li>
-        <li id="js-centered-more" class="nav-link more"><a href="javascript:void(0)">TRAILS</a>
-          <ul class="submenu">
-            <li><a href="javascript:void(0)">TRAIL STATUS</a></li>
-            <li><a href="javascript:void(0)">COCHRAN MILL</a></li>
-            <li><a href="javascript:void(0)">SOPE CREEK</a></li>
-          </ul>
-        </li>
-        <li class="nav-link"><a href="javascript:void(0)">CALENDAR</a></li>
-        <li class="nav-link"><a href="javascript:void(0)">BLOG</a></li>
-        <li class="nav-link"><a href="javascript:void(0)">MEMBERSHIP</a></li>
-        <li class="nav-link"><a href="javascript:void(0)">SHOP</a></li>
-        <li class="nav-link">
-          <input type="button" value="DONATE" class="donate-button">
-        </li>
-      </ul>
--->
     </nav>
   </div>
 </div>
