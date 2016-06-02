661a967b24335dc01678d94e24fcf9cbb8ad2640
Ian Kennedy <exposure@gmail.com>
2016-06-02 14:53:34 -0400

    added r, scaled up socials closes #44 and #45

--- wp-content/themes/sorbaatlanta/footer.php
+++ wp-content/themes/sorbaatlanta/footer.php
@@ -4,17 +4,20 @@
     <ul class="footer-copyright">
       <li>
         <h6>&copy; <?php echo date('Y'); ?> SorbaAtlanta.org</h6></li>
-      <li>design by <a href="http://www.resolutionathens.com">resolutionathens.com</a></li>
+      <li>design by <a href="http://www.resolutionathens.com">resolution</li>
+      <li><a href="http://www.resolutionathens.com" alt="resolutionathens.com"><img src="/assets/img/resolution.svg" style="width:60px; margin-top:1em;"></a></li>
     </ul>
     <ul class="footer-socials-and-logos">
       <li>
         <h6>Follow us</h6></li>
       <div class="footer-socials">
-        <a href="https://www.facebook.com/SORBA-Atlanta-Southern-Off-Road-Bicycle-Association-355177060787/"><img src="/assets/img/facebook.png"></a>
-        <a href="https://twitter.com/SORBAAtlanta"><img src="/assets/img/twitter.png"></a>
-        <a href="https://www.instagram.com/sorbaatlanta/"><img src="/assets/img/instagram.png"></a>
+        <ul>
+          <li><a href="https://www.facebook.com/SORBA-Atlanta-Southern-Off-Road-Bicycle-Association-355177060787/"><img src="/assets/img/facebook.svg"></a></li>
+          <li><a href="https://twitter.com/SORBAAtlanta"><img src="/assets/img/twitter-orange.svg"></a></li>
+          <li><a href="https://www.instagram.com/sorbaatlanta/"><img src="/assets/img/instagram.svg"></a></li>
+        </ul>
       </div>
-      <li><img src="/assets/img/IMBA-SORBA-min.png" alt="IMBA SORBA" style="width:150px;"></li>
+      <li><img src="/assets/img/IMBA-SORBA-min.png" alt="IMBA SORBA" style="width:200px;"></li>
     </ul>
     <ul class="newsletter-signup">
       <li>
