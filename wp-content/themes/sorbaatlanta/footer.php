a6bb72090bf6df2f0d1c498acb7603cf8f0e0e81
Ian Kennedy <exposure@gmail.com>
2016-06-11 08:36:58 -0400

    updating wp & plugins

--- wp-content/themes/sorbaatlanta/footer.php
+++ wp-content/themes/sorbaatlanta/footer.php
@@ -1,5 +1,5 @@
 </main>
- <footer class="footer" role="contentinfo">
+<footer class="footer" role="contentinfo">
   <div class="footer-links">
     <ul class="footer-copyright">
       <li>
@@ -12,9 +12,15 @@
         <h6>Follow us</h6></li>
       <div class="footer-socials">
         <ul>
-          <li><a href="https://www.facebook.com/SORBA-Atlanta-Southern-Off-Road-Bicycle-Association-355177060787/"><img src="/assets/img/facebook.svg"></a></li>
-          <li><a href="https://twitter.com/SORBAAtlanta"><img src="/assets/img/twitter-orange.svg"></a></li>
-          <li><a href="https://www.instagram.com/sorbaatlanta/"><img src="/assets/img/instagram.svg"></a></li>
+          <li>
+            <a href="https://www.facebook.com/SORBA-Atlanta-Southern-Off-Road-Bicycle-Association-355177060787/"><img src="/assets/img/facebook.svg"></a>
+          </li>
+          <li>
+            <a href="https://twitter.com/SORBAAtlanta"><img src="/assets/img/twitter-orange.svg"></a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/sorbaatlanta/"><img src="/assets/img/instagram.svg"></a>
+          </li>
         </ul>
       </div>
       <li><img src="/assets/img/IMBA-SORBA-min.png" alt="IMBA SORBA" style="width:200px;"></li>
@@ -23,10 +29,22 @@
       <li>
         <h6>Sign up for our newsletter</h6></li>
       <li>
-        <form action="" method="post" id="subForm">
-          <input type="email" name="email" id="email-input" placeholder="example@domain.com">
-          <input type="submit" value="SIGN ME UP!" class="button">
-        </form>
+        <!-- Begin MailChimp Signup Form -->
+        <link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css">
+        <div id="mc_embed_signup">
+          <form action="//sorbaatlanta.us13.list-manage.com/subscribe/post?u=75194a74e63c3cfc3159c7a46&amp;id=64308af89f" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+            <div id="mc_embed_signup_scroll">
+              <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+              <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+              <div style="position: absolute; left: -5000px;" aria-hidden="true">
+                <input type="text" name="b_75194a74e63c3cfc3159c7a46_64308af89f" tabindex="-1" value="">
+              </div>
+              <div class="clear">
+                <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+              </div>
+            </div>
+          </form>
+        </div>
       </li>
     </ul>
   </div>
@@ -34,4 +52,5 @@
 </footer>
 <?php wp_footer(); ?>
 </body>
+
 </html>
